fb9e754f634b18d373e1064fedd7a7926fd6bc41
Josh Bredbenner <jcbredbenner@gatech.edu>
2021-02-07 23:22:40 -0500

    Ready to be adapted to firebase

--- app/src/main/java/com/example/firstprojectmas/MainActivity.java
+++ app/src/main/java/com/example/firstprojectmas/MainActivity.java
@@ -12,24 +12,113 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int storesRegistered = 0;
+    String name;
+    EditText storeInput;
+    Button registerButton;
+    Button clearButton;
+    TextView currentStoreDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.home_screen);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        storeInput = (EditText) findViewById(R.id.editStoreName);
+        registerButton = (Button) findViewById(R.id.button_register);
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                name = storeInput.getText().toString();
+                switch (storesRegistered) {
+                    case 0:
+                        currentStoreDisplay = (TextView) findViewById(R.id.Store0);
+                        currentStoreDisplay.setText(name);
+                        break;
+                    case 1:
+                        currentStoreDisplay = (TextView) findViewById(R.id.Store1);
+                        currentStoreDisplay.setText(name);
+                        break;
+                    case 2:
+                        currentStoreDisplay = (TextView) findViewById(R.id.Store2);
+                        currentStoreDisplay.setText(name);
+                        break;
+                    case 3:
+                        currentStoreDisplay = (TextView) findViewById(R.id.Store3);
+                        currentStoreDisplay.setText(name);
+                        break;
+                    case 4:
+                        currentStoreDisplay = (TextView) findViewById(R.id.Store4);
+                        currentStoreDisplay.setText(name);
+                        break;
+                    case 5:
+                        currentStoreDisplay = (TextView) findViewById(R.id.Store5);
+                        currentStoreDisplay.setText(name);
+                        break;
+                    case 6:
+                        currentStoreDisplay = (TextView) findViewById(R.id.Store6);
+                        currentStoreDisplay.setText(name);
+                        break;
+                    case 7:
+                        currentStoreDisplay = (TextView) findViewById(R.id.Store7);
+                        currentStoreDisplay.setText(name);
+                        break;
+                }
+                if (storesRegistered <= 8) {
+                    storesRegistered++;
+                }
             }
         });
+
+        clearButton = (Button) findViewById(R.id.clear_button);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (storesRegistered) {
+                    case 1:
+                        currentStoreDisplay = (TextView) findViewById(R.id.Store0);
+                        currentStoreDisplay.setText("Deleted");
+                        break;
+                    case 2:
+                        currentStoreDisplay = (TextView) findViewById(R.id.Store1);
+                        currentStoreDisplay.setText("Deleted");
+                        break;
+                    case 3:
+                        currentStoreDisplay = (TextView) findViewById(R.id.Store2);
+                        currentStoreDisplay.setText("Deleted");
+                        break;
+                    case 4:
+                        currentStoreDisplay = (TextView) findViewById(R.id.Store3);
+                        currentStoreDisplay.setText("Deleted");
+                        break;
+                    case 5:
+                        currentStoreDisplay = (TextView) findViewById(R.id.Store4);
+                        currentStoreDisplay.setText("Deleted");
+                        break;
+                    case 6:
+                        currentStoreDisplay = (TextView) findViewById(R.id.Store5);
+                        currentStoreDisplay.setText("Deleted");
+                        break;
+                    case 7:
+                        currentStoreDisplay = (TextView) findViewById(R.id.Store6);
+                        currentStoreDisplay.setText("Deleted");
+                        break;
+                    case 8:
+                        currentStoreDisplay = (TextView) findViewById(R.id.Store7);
+                        currentStoreDisplay.setText("Deleted");
+                        break;
+                }
+                if (storesRegistered > 0) {
+                    storesRegistered--;
+                }
+            }
+        });
+
     }
 
     @Override
@@ -42,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
+        // automatically handle clicks on the Home/Up clear_button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
